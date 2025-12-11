cb32cf3ff8a93d2afae22c5b927534db09529093
Jeremy McWilliams <jeremym@lclark.edu>
2025-12-11 08:43:00 -0800

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,53 @@
+import {ApplicationRef, DoBootstrap, Injector, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {createCustomElement, NgElementConstructor} from "@angular/elements";
+import {Router} from "@angular/router";
+import {selectorComponentMap} from "./custom1-module/customComponentMappings";
+import {TranslateModule} from "@ngx-translate/core";
+import { CommonModule } from '@angular/common';
+import { AutoAssetSrcDirective } from './services/auto-asset-src.directive';
+import {SHELL_ROUTER} from "./injection-tokens";
+
+
+
+export const AppModule = ({providers, shellRouter}: {providers:any, shellRouter: Router}) => {
+   @NgModule({
+    declarations: [
+      AppComponent,
+      AutoAssetSrcDirective
+    ],
+    exports: [AutoAssetSrcDirective],
+    imports: [
+      BrowserModule,
+      CommonModule,
+      TranslateModule.forRoot({})
+    ],
+    providers: [...providers, {provide: SHELL_ROUTER, useValue: shellRouter}],
+    bootstrap: []
+  })
+  class AppModule implements DoBootstrap{
+    private webComponentSelectorMap = new Map<string,  NgElementConstructor<unknown>>();
+
+    constructor(private injector: Injector, private router: Router) {
+      router.dispose(); //this prevents the router from being initialized and interfering with the shell app router
+    }
+
+    ngDoBootstrap(appRef: ApplicationRef) {
+      for (const [key, value] of selectorComponentMap) {
+        const customElement = createCustomElement(value, {injector: this.injector});
+        this.webComponentSelectorMap.set(key, customElement);
+      }
+    }
+
+    /**
+     * Use componentMapping, selectorComponentMap
+     * @param componentName
+     */
+    public getComponentRef(componentName:string) {
+      return this.webComponentSelectorMap.get(componentName);
+    }
+  }
+  return AppModule
+}
+
