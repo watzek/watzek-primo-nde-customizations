f256115d4ac91e2588aed3154e2ecad77ae58085
Jeremy McWilliams <jeremym@lclark.edu>
2025-12-11 09:21:33 -0800

    added chat widget customization, working

--- app/custom1-module/customComponentMappings.ts
+++ app/custom1-module/customComponentMappings.ts
@@ -1,5 +1,12 @@
-// Define the map
+// import additional custom modules
+
+import {SlaaskChatwidgetComponent} from '../slaask-chatwidget/slaask-chatwidget.component';
+
+
+// Now map them
 export const selectorComponentMap = new Map<string, any>([
+
+    ['nde-footer-after', SlaaskChatwidgetComponent]
 
 
 
--- app/slaask-chatwidget/slaask-chatwidget.component.ts
+++ app/slaask-chatwidget/slaask-chatwidget.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var _slaask: any;
 
 @Component({
   selector: 'custom-slaask-chatwidget',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './slaask-chatwidget.component.scss'
 })
 export class SlaaskChatwidgetComponent {
+  ngOnInit(): void {
+    // Check if script is already added
+    if (!document.querySelector('script[src*="slaask.js"]')) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://libapps.s3.amazonaws.com/sites/5089/include/slaask.js';
+      script.onload = () => {
+        // Wait for the script to load before initializing
+        if (window.hasOwnProperty('_slaask')) {
+          (_slaask as any).init('295dd444a1af2c9ab3456b7da45a415a');
+        }
+      };
+      document.head.appendChild(script);
+    } else {
+      // Script already exists, just init again
+      (_slaask as any).init('295dd444a1af2c9ab3456b7da45a415a');
+    }
+  }
 
 }
