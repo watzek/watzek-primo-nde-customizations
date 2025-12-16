e2a014fddac0732f874d877ad60784e2913dfd4d
Jeremy McWilliams <jeremym@lclark.edu>
2025-12-16 10:07:29 -0800

    report a problem working

--- app/report-problem/report-problem.component.ts
+++ app/report-problem/report-problem.component.ts
@@ -2,12 +2,9 @@ import { selectFullDisplayRecord } from '../utils/fullDisplayRecordSelector';
 import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CommonModule } from '@angular/common';
-//import {  HttpClient } from '@angular/common/http';
-
-
 
 @Component({
-    selector: 'custom-nde-mms-id-display',
+    selector: 'custom-report-problem',
     standalone: true,
     imports: [CommonModule],
     templateUrl: './report-problem.component.html',
@@ -15,22 +12,20 @@ import { CommonModule } from '@angular/common';
 })
 export class ReportProblemComponent {
 
-    @Input() parentCtrl!: any; // The ! tells TypeScript: “I know this will be initialized before it's used.”
+   // @Input() parentCtrl!: any; // The ! tells TypeScript: “I know this will be initialized before it's used.”
 
-    selectedRecordId: string | undefined;
 
     docid: string | undefined;
+    context: string | undefined;
 
+    vid: string | null = this.getUrlParameter('vid');
 
-    izShow = true;
-    nzShow = true;
-    izLabel = 'Alma IZ Record No.';
-    nzLabel = 'Alma NZ Record No.';
-    izSuffix = '1452';
-    instCode = '01ALLIANCE_UW';
+    getUrlParameter(parameterName: string): string | null {
+        const urlParams = new URLSearchParams(window.location.search);
+        console.log(urlParams.get(parameterName));
+        return urlParams.get(parameterName);
+    }
 
-    izMmsid: string | null = null;
-    nzMmsid: string | null = null;
 
     constructor(private store: Store, private el: ElementRef) { }
 
@@ -38,18 +33,14 @@ export class ReportProblemComponent {
         this.store.select(selectFullDisplayRecord).subscribe((record) => {
             console.log('Record:', record);
 
-            this.docid=record.origRecordId;
+           // this.docid=record.origRecordId;
+            //this.docid = record?.origRecordId; //doesn't work for all records
+            this.docid = record?.pnx?.control?.recordid[0]; //works reliably
+
+            console.log(this.docid);
+            this.context = record?.context;
+            //this.vid=this.vid; //I guess redundant?
             
-
-            const pnx = record?.pnx?.control;
-            console.log('PNX:', pnx);
-            const pnxfull = record?.pnx?.addata?.url;
-            console.log('PNX Full:', pnxfull);
-            // Extract the source record ID from the record's PNX control field
-           // const recordId = record?.pnx?.control?.sourcerecordid;
-            //console.log(recordId);
-
- 
             const ndeSelector = 'nde-full-display-service-container-before-from-remote-0';
             // The actual NDE element this instance belongs to
             const hostNDE = this.el.nativeElement.closest(ndeSelector);
@@ -60,69 +51,11 @@ export class ReportProblemComponent {
             const isFirst = (allTargets[0] === hostNDE);
 
             if (!isFirst) {
+                //hides the ones that aren't first - element repeats
                 hostNDE.style.display = 'none';
             }
- 
- 
- 
+
  
         });
-
-        console.log('Source Record ID:', this.selectedRecordId);
-        /*         if (this.selectedRecordId && this.selectedRecordId.startsWith('99')) {
-                    this.izShow = true;
-                    this.nzShow = true;
-                } */
-
-        //srcid is nz mmsid, implies no iz mmsid        
-        if (this.selectedRecordId && this.selectedRecordId.startsWith('99') && !this.selectedRecordId.endsWith(this.izSuffix)) {
-            this.nzShow = true;
-            this.izShow = false;
-            this.nzMmsid = this.selectedRecordId ?? null;
-
-
-            //srcid is iz mmsid, check sru for nz mmsid
-        }
-        if (this.selectedRecordId && this.selectedRecordId.startsWith('99') && this.selectedRecordId.endsWith(this.izSuffix)) {
-            this.izShow = true;
-            this.izMmsid = this.selectedRecordId ?? null;
-            this.sruCall(this.selectedRecordId);
-        }
-
-        //neither iz nor nz mmsid
-        if (!this.selectedRecordId || !this.selectedRecordId.startsWith('99')) {
-            this.nzShow = false;
-            this.izShow = false;
-        }
-    }
-
-    private sruCall(mmsid: string): void {
-        const url = `https://na01.alma.exlibrisgroup.com/view/sru/${this.instCode}?version=1.2&operation=searchRetrieve&query=alma.mms_id=${mmsid}`;
-        console.log('SRU URL:', url);
-       /*
-       
-        this.http.get(url, { responseType: 'text' }).subscribe(response => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(response, 'text/xml');
-            const fields = xmlDoc.getElementsByTagName('datafield');
-
-            let found = false;
-            for (let i = 0; i < fields.length; i++) {
-                const field = fields[i];
-                if (field.getAttribute('tag') === '035') {
-                    const subfield = field.getElementsByTagName('subfield')[0]?.textContent;
-                    if (subfield?.includes('(EXLNZ-01ALLIANCE_NETWORK)')) {
-                        const pieces = subfield.split(')');
-                        this.nzMmsid = pieces[1];
-                        console.log('NZ MMSID:', this.nzMmsid);
-                        found = true;
-                        break;
-                    }
-                }
-            }
-            if (!found) {
-                this.nzShow = false;
-            }
-        });*/
-    }
+    }   
 }
