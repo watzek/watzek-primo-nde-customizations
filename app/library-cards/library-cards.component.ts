da9c5a951783c153c129fd04751e5a86ec47c674
jeremymcwilliams <jeremym@lclark.edu>
2025-12-18 15:21:37 -0800

    dynamic hours for b & w

--- app/library-cards/library-cards.component.ts
+++ app/library-cards/library-cards.component.ts
@@ -12,27 +12,42 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class LibraryCardsComponent {
   
 
-  hoursText: string | null = null;
+  watzekHoursText: string | null = null;
+  boleyHoursText: string | null = null;
   error = false;
 
 
-  private hoursUrl =
+  private watzekHoursUrl =
     'https://libcal.lclark.edu/api_hours_today.php?iid=3636&lid=4016&format=json';
+
+  private boleyHoursUrl=  
+    'https://lawlib.lclark.libcal.com/api_hours_today.php?lid=1768&format=json&systemTime=0';
 
   constructor(private http: HttpClient) {}
   
 
     ngOnInit() {
-      /*
-    this.http.get<any>(this.hoursUrl).subscribe({
+      
+    this.http.get<any>(this.watzekHoursUrl).subscribe({
       next: (data) => {
-        this.hoursText = data?.locations?.[0]?.rendered ?? 'Hours unavailable';
+        this.watzekHoursText = data?.locations?.[0]?.rendered ?? 'Hours unavailable';
       },
       error: () => {
         this.error = true;
       }
     });
-    */
+
+    this.http.get<any>(this.boleyHoursUrl).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.boleyHoursText = data?.locations?.[1]?.rendered ?? 'Hours unavailable';
+      },
+      error: () => {
+        this.error = true;
+      }
+    });
+
+    
   }
   
 
