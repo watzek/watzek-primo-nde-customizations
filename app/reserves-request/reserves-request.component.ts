844ffecc8d2205305df72d7ef27947148fefe31f
Jeremy McWilliams <jeremym@lclark.edu>
2026-01-14 10:50:44 -0800

    a not terrible form in dialog, passing data back to class

--- app/reserves-request/reserves-request.component.ts
+++ app/reserves-request/reserves-request.component.ts
@@ -5,11 +5,12 @@ import {MatDialog,
   MatDialogTitle,
   MatDialogContent, MatDialogModule} from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
+//import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+//import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'custom-reserves-request',
@@ -76,15 +77,29 @@ export class ReservesRequestComponent {
   selector: 'reserves-request-dialog',
   templateUrl: 'reserves-request-dialog.html',
   standalone: true,
+  styleUrl: './reserves-request.component.scss',
     imports: [
-    MatDialogTitle, 
-    MatDialogContent, 
+
     FormsModule,
-    MatFormFieldModule,  // Add this
-    MatInputModule       // You'll likely need this too for input fields
+   // MatFormFieldModule,  // Add this
+    MatInputModule ,
+   // MatSelectModule ,
+    MatDialogModule     // You'll likely need this too for input fields
   ],
 })
 export class ReservesRequestDialog {
   data = inject(MAT_DIALOG_DATA);
+    courseDetails = {
+    course: '',
+    courseTitle: '',
+    loanPeriod: '',
+    comments: ''
+  };
+
+    submitForm(form: any): void {
+    if (form.valid) {
+      console.log('Form data:', this.courseDetails);
+    }
+  }
 }
 
